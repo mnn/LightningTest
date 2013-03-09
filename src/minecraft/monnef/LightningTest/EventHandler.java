29735de1402be9d4fce030b08e184762320a1953
moen <moen@366.hopto.org>
2013-03-09 13:02:54 +0100

    Updated to pre-1.5.

--- src/minecraft/monnef/LightningTest/EventHandler.java
+++ src/minecraft/monnef/LightningTest/EventHandler.java
@@ -1,31 +1,31 @@
 package monnef.LightningTest;
 
-import java.util.Random;
-
 import net.minecraft.block.Block;
 import net.minecraftforge.event.Event.Result;
 import net.minecraftforge.event.ForgeSubscribe;
 import net.minecraftforge.event.world.LightningGeneratedEvent;
 
+import java.util.Random;
+
 public class EventHandler {
-	private static Random rand = new Random();
+    private static Random rand = new Random();
 
-	@ForgeSubscribe
-	public void HandleLightning(LightningGeneratedEvent event) {
-		System.out.print(String.format("lightning at %d %d %d ~ ", event.x,
-				event.y, event.z));
-		boolean remove = rand.nextBoolean();
-		int bId;
+    @ForgeSubscribe
+    public void HandleLightning(LightningGeneratedEvent event) {
+        System.out.print(String.format("lightning at %d %d %d ~ ", event.x,
+                event.y, event.z));
+        boolean remove = rand.nextBoolean();
+        int bId;
 
-		if (remove) {
-			bId = Block.cake.blockID;
-			System.out.println("bolt removed");
-			event.setResult(Result.DENY);
-		} else {
-			bId = Block.wood.blockID;
-			System.out.println("bolt stays");
-		}
+        if (remove) {
+            bId = Block.cake.blockID;
+            System.out.println("bolt removed");
+            event.setResult(Result.DENY);
+        } else {
+            bId = Block.obsidian.blockID;
+            System.out.println("bolt stays");
+        }
 
-		event.world.setBlock(event.x, event.y, event.z, bId);
-	}
+        event.world.setBlockAndMetadataWithNotify(event.x, event.y, event.z, bId, 0, 3);
+    }
 }
